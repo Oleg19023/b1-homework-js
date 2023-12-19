1b39e516e1943c6deee059057f27a318cbe9b5dc
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-12-19 18:27:54 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+function convertToICAO() {
+    const inputText = document.getElementById("inputText").value.toUpperCase();
+    const outputText = document.getElementById("outputText");
+
+    const icaoMap = {
+        'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie',
+        'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot',
+        'G': 'Golf', 'H': 'Hotel', 'I': 'India',
+        'J': 'Juliet', 'K': 'Kilo', 'L': 'Lima',
+        'M': 'Mike', 'N': 'November', 'O': 'Oscar',
+        'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo',
+        'S': 'Sierra', 'T': 'Tango', 'U': 'Uniform',
+        'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray',
+        'Y': 'Yankee', 'Z': 'Zulu',
+        '0': 'Zero', '1': 'One', '2': 'Two',
+        '3': 'Three', '4': 'Four', '5': 'Five',
+        '6': 'Six', '7': 'Seven', '8': 'Eight',
+        '9': 'Nine'
+    };
+
+    const decodedWords = Array.from(inputText).map(char => icaoMap[char] || char);
+    outputText.textContent = decodedWords.join(' ');
+}
